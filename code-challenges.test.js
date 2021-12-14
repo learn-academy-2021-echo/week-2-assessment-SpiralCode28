cf310466a1408cb5318d5192a1f62a74a820c4a6
spiralcode28 <learnacademy@LEARNs-MacBook-Air.local>
2021-12-14 00:14:39 -0800

    finished as much as I could with the extra day, splitting headache, will try to finish the Interview Questions before class.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,22 +12,41 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
-// Expected output: "15 is divisible by three"
-var num2 = 0
-// Expected output: "0 is divisible by three"
-var num3 = -7
-// Expected output: "-7 is not divisible by three"
+// Write a test using describe and expect statements that will show the expected outcome for each varible.
 
+describe("Variable is or is not divisible by three", () => {
+    
+    var num1 = 15
+    // Expected output: "15 is divisible by three"
+    
+    var num2 = 0
+    // Expected output: "0 is divisible by three"
 
+    var num3 = -7
+    // Expected output: "-7 is not divisible by three"
 
+    it("returns a string that says divisible by three or not divisible by three value", () => {
+        // Use expect statements
+        expect(divisibleByThree(num1)).toEqual("15 is divisible by three")
+                // There was the proper red error for all expect statements (GOOD) -- After writing the function, there was another error that said "expected 15..." "${num} is divisible by three" (not matching but I thought it was how you wrote it)
+        expect(divisibleByThree(num2)).toEqual("0 is divisible by three")
+                
+        expect(divisibleByThree(num3)).toEqual("-7 is not divisible by three")
+
+    })
+})
+    
 // b) Create the function that makes the test pass.
 
+    // Write a function that passes the test. The function will take in each variable, one at a time. Used the modulus sign to give us the remainder, if any. If number returns a remainder of 0, it is divisible by three. If there's another value remainding, it is not divisible by three.
+    const divisibleByThree = (num) => {
+        return num % 3 === 0 ? `${num} is divisible by three` : `${num} is not divisible by three`
+    }
+        // I went back and reviewed the code block, found I had put the single quotes outside of the return. Replaced with the correct ``marks and now test runs green.
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
@@ -47,6 +68,7 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 
 // a) Create a test with expect statements for each of the variables provided.
 
+    // describe("Divisble by three")
 var vowelTester1 = "learn"
 // Expected output: 1
 var vowelTester2 = "academy"
@@ -55,5 +77,25 @@ var vowelTester3 = "challenges"
 // Expected output: 2
 
 
+// ex.)
+
+    // describe("test a function that returns relax or keep going", () => {
+
+    //     it("Checks each number if it has a remainder using a modulus sign ... if it is divisible by 3 or not divisible by 3", () => {
+
+    //         expect(stressed(true)).toEqual("relax")
+    //         expect(stressed(false)).toEqual("keep going")
+    //     })
+    // })
+
 
 // b) Create the function that makes the test pass.
+// possible use:
+ // for()
+    // forEach() 
+
+    // Step 1.) create a test with expect statements 
+        // Step 1.) using psuedocode
+    // Step 2.)
+    // Step 3.) refactor (make cleaner)
+// (describe can be more simple than the expect) 
--- interview-questions.md
+++ interview-questions.md
@@ -9,16 +9,25 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is the difference between a parameter and an argument?
 
   Your answer:
+  
+  - The difference between a parameter and an argument is that parameters are variables which are in a declaration of functions. The argument is when a value is called by the function.
 
   Researched answer:
+  
+  - PARAMETER is like a placeholder, belonging to the function naming and is used in the function body
+  - ARGUMENT is the actual value (This means an actual value which is passed by the function calling)
 
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
   Your answer:
+  
+  - The .map() method goes through each value in an array, incrementally by +1 whole number until it reaches the last value. It will print all numbers. 
 
   Researched answer:
+
+  - 
 
 
 
@@ -26,7 +35,11 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
+  - 
+
   Researched answer:
+
+  - 
 
 
 
@@ -34,15 +47,22 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
+  - While a function is code written to run a set of tasks, a method is within a function that has a task to be called on by the function.
+
   Researched answer:
 
+  - A method belongs to a class and an object. It is a function within a function. A function is a section of code thats performs a specific set of tasks. We can define a function using the function keyword, name and parameters.
 
 
 5. What is the difference between a class and an object?
 
   Your answer:
 
+  - A class is like a blueprint, an object is more specific. 
+
   Researched answer:
+
+  - 
 
 
 
